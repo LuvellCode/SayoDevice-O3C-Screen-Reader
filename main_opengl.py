fb4d1208b7aa76dbedae21f696d05284cf95629c
LuvellCode <igor4ikdidukh@gmail.com>
2025-04-11 21:33:33 +0300

    Added Icon for OpenGL implementation. Build testing

--- main_opengl.py
+++ main_opengl.py
@@ -9,6 +9,15 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import glfw
 
+from PIL import Image
+
+# Icon only
+import base64
+from io import BytesIO
+
+ICON = "AAABAAEAIBcAAAEAIAAEDAAAFgAAACgAAAAgAAAALgAAAAEAIAAAAAAAgAsAAAAAAAAAAAAAAAAAAAAAAAAZHCH/Ghsl/w4JCf8OCwz/DgwM/xAKCf8YGB//GBMX/xUODf8YExT/CgYI/2xrb/9CQET/ERAR/252e/8qKi3/EQwL/xUTFP8UEhP/GBQT/xsWFf8ZFRX/ERAS/w0LDf8kJiz/SlZl/0VQX/9ATFr/OUVT/zZEUv8fJi//ExYd/xYXGf8VExP/Eg0N/xAMDP8QDAz/EQ0N/w8KCv8TDQ3/FhIR/xYUFf8VFBb/RUdL/3h9g/8ODhD/R0pO/1BVWv81MjP/GBQU/xUTFf8YFRX/HRkX/xsXFv8PDhD/DAsN/xwcIP9GTl3/LjQ+/zU7Rf8xNTz/Jygt/xkZHf8XFhj/FRca/xYXGP8UEhL/EA0O/xEMDv8QDA3/EQ8P/xQQEP8XFRf/DAwO/05RVv+Kk5r/ho6V/1FVWv9scnf/anN6/3N5ff8oJif/EQ4Q/xgWFv8eGRf/HhoZ/xEQEf8PDQ7/FRQW/z9HVP8pLjj/FRMW/xUQD/8PCQj/DQgJ/xIOD/8WGBn/Fxkc/x4hJf8VFBX/EA0N/xAND/8REBD/ExER/xQTFf8TFBb/FhcY/xwcH/8jJCf/Ghod/0NHS/8/Qkj/eoOK/z9AQ/8NCQv/GRcY/x0ZGP8dGRj/Eg8Q/w0LDf8QDhD/KCgu/ycpMf8SDhD/Ew4N/xIODv8RDQ7/CgoO/xcbIP8mLzj/KC43/xYUFv8SEBD/EQ8R/xMREv8TEhT/ExMV/xMUFv8TFBb/EhIU/xEQEv8TEhb/DQsN/wkGCP83OT3/SEpO/w0KC/8YFxf/GRYV/xoWFv8UERH/CggK/w4MDv8tHBP/MBwS/yYYEv8aEA3/HRIP/xcPDf8QDQ7/P0RN/zk9SP8vLzf/FhIS/xUSFP8UERT/FBIU/xQTFv8UFBb/ExMV/xQTFf8TExX/EhAR/xEOD/8TERP/FBQW/xQVGP8eHiH/FBQV/xYWFv8VFBT/EhAR/xYSEv8LCQr/BwcI/0UrHf9jOyX/Nh8V/xYMCf8VCgf/EAkI/xIPEv+cmqH/U0lP/zM0Qf8rKDP/FRIT/xYVF/8UExb/FBUY/xQUF/8TFBb/FRQY/xIRFP8SFBv/Fhok/xMTHP8QERT/EhIV/xMUF/8VFhn/FRUX/xUUFf8RDxH/ExAR/xINDv8CAwT/Qywf/2Q+Kf8WEhX/DQwQ/xAMDf8QDQ7/FBQX/6Shp/9UUmD/KCYt/01Sb/8bGR3/FRQW/xcYHP8YGR3/FRYZ/xUVGP8VFBj/EBAV/yo3Tv9IX4T/R1+H/yo4Uv8SEhf/FBUX/xUWGf8WFxj/FBQV/xMREv8UEBD/Eg0O/xwbI/8pISP/VjIe/zUfFf8iGxz/Ix4g/xUTFv8ZGR3/mp2n/1tgev8dGyH/NTdJ/zY6UP8hIyv/GBkb/xgaHv8WFxz/Fhca/w0MDv8fJDT/UHCf/2aSzv9qmtr/VYO8/x4pQf8ODRD/FRcb/xUWGf8UExb/FBET/xIOEP80M0L/TE5k/z5CWP88OUT/SC8m/zYbEf8XCwr/DQgI/xgVFf+NmKX/ZWqJ/y8pM/8nJi//YWmT/1Ffh/80QFf/Iigx/xgYG/8QEBH/DgwQ/0FTdf9TebP/VnLD/1Vzwv9Yh8X/RWaX/xQWIf8UFhr/FRYa/xQUF/8QDA3/Jygz/2R0mP9da47/U1x8/0ZPbP86QVj/MC45/ykmKP8oIin/MSs2/36Upv9cbJD/ZlRj/1lUb/9cZY//T2OQ/01pnP9IZJL/N0lo/yAmNP8vNUz/Xn+2/16Du/9Ta6b/YIG9/2aT0P9ehsL/Lz5f/xESF/8WGR//FBUY/w4KC/89SF3/d5XC/3GJs/9leqP/VWSI/0NOa/8/SGD/IyMo/w0KFP8jIDD/fZaq/0VjkP9GSFz/SlZ7/1Fpmv9JZpf/SWye/0psoP9KbqT/R2uf/1Jjiv9rga7/WHm2/3ue2f9vmNj/XH+7/3iRwP9FWIP/FBgh/yAmLf8cHyX/DgsN/z5PZ/97oNH/eZvL/3CPvf9jfqf/V2yT/z9Ocv9ARVb/Y1tF/0xENP9/l6z/QGeZ/y5GZP84V4f/Qmuj/0JqoP9Eap//SHKm/0t3q/9KcqX/YXOf/3uWy/9jhsP/f6fk/3yp4v9nj83/c5jW/16Dvv8rNE3/FRQW/0Rgdf8rNTv/MkFV/3Gf1v90n9H/a5bI/2mMuv9qibb/WnOe/0FSd/9VVFj/XlJD/3yXr/8+bKD/LEpy/zJZjv8/drD/QHOq/zxpo/9Cd7D/P3q2/zxejP9mgKz/XHam/1p3p/9khbf/ZIi7/1l2pv9ZcJr/Z5LN/zpLaf8REBP/aY+p/0dVW/9DTUr/UXuk/1OJxf9HcKP/W4Kx/22Twf9qjLn/V3al/0RZhP9NUVT/fZmy/ztnm/8mLz//L1J+/z9+uv9AebD/PG+o/z96tv86dK//IjFJ/111nP9UZoj/Slp8/26Kwf9visb/SGGP/1RtlP9ZhL3/KkRq/ywxO/+AqMT/VXB//725lf/g4Ln/gY6S/yQ1Xv9OapX/XIKu/1+Ds/9fhrr/WXuu/0xfgP98mbL/PGyh/ztnk/84bKD/QoK7/0B9tf88dK3/RIPA/z5qnf8Ej7b/LprK/1NhjP99ndD/esz+/0zL/v+Ptef/hJzC/16Owv8MuOj/BjpK/3GHnf9hhp7/9fLJ/9fXwP9SXnb/Xnum/2uPvP9slcf/bpvQ/3Cd1f9bhb7/Umub/3uas/88b6P/Onmz/zh0sP9Cgbr/P323/zx1r/9EiMj/NE9//wCPtP8nvvP/d5nQ/5K99/9Jx/n/AMz9/53R//+x1Pz/hbzn/wDQ//8FLj7/eo6k/3mbr/+Mj4T/VnKb/2eQw/97pdX/f6zd/36w4v90qd//aZvX/2SR0P9yjbr/e5u2/z9ypv86d6//N3Ov/0ODvf9Bfbn/O3Wy/0WJyv87YZX/CEph/xXO//93odX/ibHn/yzL//8B1f//dsn4/73I7f962f//AKPO/yEiLP+Eo7n/XHaT/0Zol/9umcz/eaXW/3qr3f98r+H/d6vf/3Sp4P9yqeb/e6jd/2d/nf98nbn/RHir/zh1rv81baj/PXmz/z53s/8+e7f/RIO//0qIxf8lNFH/AXmX/0e27P9Qsuj/Ha3d/zSVwv8qvu7/e7nk/yjC7/8MQVn/LC89/0JfgP9Jbp3/bZrN/3Sj2P93q+H/e7Hl/4G46v+GvvD/fK/a/2aNrv9YdZH/T2N4/3yfu/9Bd6v/O3q0/0F8t/9CgLv/Q4O//0iKxv9Pjsn/UpTQ/0yIwf8tQV//IGSI/xRXdf8bJTT/Kic3/xs+T/8eaYb/LWaS/yI1Vv8vTnL/Un2v/2eaz/9xqd7/gb3u/5DN9v+X0ff/jMLp/3SixP9Zfpn/TGuC/01qg/9NZXz/e567/zx0q/84eLP/OHm2/zp+vf89gb//QoG9/0GGx/9Hjc3/UZXU/12a1v9PfrL/T2qP/ypEXf8pS2X/M0tp/0pjh/9ikcn/T4jD/2+l1/+Gv+r/j8jv/5XN8P+WzOr/jsDc/3+syv9wlrP/Zoql/1+EoP9We5b/SmmE/0dheP96nrv/Onau/zh3tP81d7b/OXy8/zyBwP9DhMH/P4XH/0eOz/9MkND/TJDQ/1ec2f+dx+j/lMHk/4y64f+NvOn/msnv/5zJ7v+Uxuz/pdXz/5/O6v+YxeH/kLvY/4Wxz/99p8b/eKC+/3GYtf9mjqr/WoGd/1B1kP9GZ4H/RV92/3OYtv85c6r/OXSu/zVuqf84c67/PHm1/0iHwv9IiMP/S43J/1GV0f9QltX/VpzZ/4quyP+vzuL/veD1/7ba8P+y1+3/q9Pq/6jQ5/+fyOL/mMPe/5K+2/+MuNb/hLHP/3ynxv91nbv/bJOv/2GIpP9XfJj/THCL/0NhfP9CWnD/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
+
+
 HID_DEFAULT_UPDATE_FREQUENCY = 200  # Hardcoded 200, so 1/200 will allow us to display 144 FPS while not pinging the device too often
 
 class HIDListener:
@@ -76,6 +85,9 @@ class HIDListener:
             print("Failed to create GLFW window")
             return False
         
+        # Icon
+        glfw.set_window_icon(self.window, 1, Image.open(BytesIO(base64.b64decode(ICON))))
+
         # Make the window's context current
         glfw.make_context_current(self.window)
         
